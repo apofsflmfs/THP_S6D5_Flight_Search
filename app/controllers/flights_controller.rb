112d75bb37a8ac29e9b7a25a6a4437c6d69528c0
Clem Baldy <clemoun@yahoo.fr>
2018-11-09 20:00:04 +0100

    2ème formulaire mis en page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,10 +7,13 @@ class FlightsController < ApplicationController
       @flights = Flight.where(departure_airport_id: dep, arrival_airport_id: arr, departure_date: (dat.to_date.midnight..dat.to_date.midnight+1.day))
       unless @flights.count >0
         @flights = "empty"
-        puts "EMPTY!!!"
       end
       render 'index'
     end
+  end
+
+  def show
+    @flights = Flight.all
   end
 end
 # 
