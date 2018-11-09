f93b729668ce1670866814c0ab655d31dda68d63
Clem Baldy <clemoun@yahoo.fr>
2018-11-09 17:39:51 +0100

    avec modèles passengers et booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Booking.all.destroy_all
+Passenger.all.destroy_all
 Airport.all.destroy_all
 Flight.all.destroy_all
 
@@ -5,12 +7,26 @@ def time_rand(from = 0.0, to = Time.now)
   Time.at(Time.now + rand(1..(7*24*60*60)))
 end
 
-codes = ["ORY", "AMS", "LHR", "SIN", "CGK", "DXB", "MAD", "GIG", "CDG", "SFO"]
+codes = ["ORY", "AMS", "CDG", "SFO"]
 
 codes.each do |code|
   Airport.create(airport_code: code)
 end
 
-1000.times do |n|
+100.times do |n|
   Flight.create(duration: rand(100..1000), departure_date: time_rand, departure_airport_id: rand(Airport.first.id..Airport.last.id), arrival_airport_id: rand(Airport.first.id..Airport.last.id))
 end
+
+3000.times do |n|
+  Passenger.create(name: "José #{n}")
+end
+
+1000.times do |n|
+  b = Booking.new
+  b.flight = Flight.find(rand(Flight.first.id..Flight.last.id))
+  b.passengers << Passenger.find(Passenger.first.id+n)
+  b.passengers << Passenger.find(Passenger.first.id+1000+n)
+  b.passengers << Passenger.find(Passenger.first.id+2000+n)
+  b.save
+end
+
